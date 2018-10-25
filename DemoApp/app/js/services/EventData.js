22a45b7acf0f5358958a0b128867da5551a4f278
Joshua Christensen <joshuac@e3672.ksd.akamaiedge.net>
2018-10-25 12:04:11 -0600

    updated to mod 4

---
+++ DemoApp/app/js/services/EventData.js
@@ -0,0 +1,20 @@
+eventsApp.factory('eventData', function () {
+    return {
+        event: {
+            "firstName": "Josh",
+            "lastName": "Christensen",
+            "imageUrl": "/img/angularjs-logo.png",
+            Sessions: [
+                {
+                    name: "Session Name"
+                },
+                {
+                    name: "Sessions Name 2"
+                },
+                {
+                    name: "Session Name 3"
+                }
+            ]
+        }
+    }
+});
